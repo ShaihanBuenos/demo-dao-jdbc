18fc87d6529744ba88afef2249e88b5f2edff4ea
ShaihanBuenos <shaihan.buenos@edu.pucrs.br>
2020-09-01 15:43:20 -0300

    Seller Created

--- src/application/app.java
+++ src/application/app.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class app {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(14, "Shaihan", "shaihan@hotmail.com", new Date(20, 6, 2000), 1200.0 , obj);
+		System.out.println(seller);
 	}
 }
