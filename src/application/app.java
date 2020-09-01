d1afc37e30176f606242344ce3c5356ff4424ed9
ShaihanBuenos <shaihan.buenos@edu.pucrs.br>
2020-09-01 17:01:54 -0300

    FindById Create

--- src/application/app.java
+++ src/application/app.java
@@ -1,16 +1,17 @@
 package application;
 
-import java.sql.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class app {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(14, "Shaihan", "shaihan@hotmail.com", new Date(20, 6, 2000), 1200.0 , obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 }
